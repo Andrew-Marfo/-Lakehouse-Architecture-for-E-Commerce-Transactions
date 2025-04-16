0445fef1586f37ddffb9e7fe3e3532844694a515
Andrew Marfo <andrewmarfo23@gmail.com>
2025-04-16 09:50:48 +0000

    modified scripts for moving files in s3

--- scripts/glue_jobs/utils/s3_utils.py
+++ scripts/glue_jobs/utils/s3_utils.py
@@ -7,16 +7,26 @@ logger = logging.getLogger('s3_utils')
 
 s3_client = boto3.client('s3')
 
-def move_file(bucket, source_key, dest_key):
+def move_files(bucket_name, source_path, dest_path):
     """
-    Move a file from source_key to dest_key in the same S3 bucket.
+    Move all files from source_path to dest_path in the S3 bucket.
     """
     try:
-        logger.info("Copying file from %s to %s in bucket %s", source_key, dest_key, bucket)
-        s3_client.copy_object(Bucket=bucket, CopySource={'Bucket': bucket, 'Key': source_key}, Key=dest_key)
-        logger.info("Deleting original file: %s", source_key)
-        s3_client.delete_object(Bucket=bucket, Key=source_key)
-        logger.info("Successfully moved file to %s", dest_key)
+        logger.info("Listing files in bucket %s with prefix %s", bucket_name, source_path)
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=source_path)
+        if 'Contents' not in response:
+            logger.warning("No files found with prefix %s", source_path)
+            return
+
+        for obj in response['Contents']:
+            source_key = obj['Key']
+            dest_key = source_key.replace(source_path, dest_path, 1)
+            logger.info("Copying file from %s to %s in bucket %s", source_key, dest_key, bucket_name)
+            s3_client.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': source_key}, Key=dest_key)
+            logger.info("Deleting original file: %s", source_key)
+            # s3_client.delete_object(Bucket=bucket_name, Key=source_key)
+            logger.info("Successfully moved file to %s", dest_key)
+
     except Exception as e:
-        logger.error("Error moving file from %s to %s: %s", source_key, dest_key, str(e))
+        logger.error("Error moving files from %s to %s: %s", source_path, dest_path, str(e))
         raise
